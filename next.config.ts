40a9e9cc40d63a9f28ae3b92e093e9e161d21930
Wizix <udinandrey666@gmail.com>
2025-12-03 20:11:22 +0400

    Add redirect for /vivaapp to bypass Telegram WebApp cache

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts');
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/vivaapp",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
