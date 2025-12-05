9234c1b0e2c4a1146ff71c4de577903ed9d60650
Wizix <udinandrey666@gmail.com>
2025-12-05 18:51:28 +0400

    add missing telegram components and hook for webapp

---
+++ src/components/telegram/WebAppMainScreen.tsx
@@ -0,0 +1,10 @@
+﻿"use client";
+
+export default function WebAppMainScreen() {
+  return (
+    <div className='p-6 text-white'>
+      <h1 className='text-xl'>VIVA Mini App</h1>
+      <p>WebAppMainScreen загружен успешно.</p>
+    </div>
+  );
+}
